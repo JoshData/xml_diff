e7d07ec67c9c0a21fe266ba32eec33b89ff54f1d
Joshua Tauberer <jt@occams.info>
2014-06-30 12:06:32 -0400

    allow overriding the word separator and the cleanup_semantic argument to diff_match_patch and re-do the simplification function to not miss various cases of simplifiable things

--- xml_diff/__init__.py
+++ xml_diff/__init__.py
@@ -15,13 +15,24 @@ import diff_match_patch
 # See below. This is a code point in the Unicode private use area.
 node_end_sentinel = "\uE000"
 
-def compare(doc1, doc2, make_tag_func=None, tags=('del', 'ins'), merge=False):
+def compare(
+	doc1, doc2,
+	make_tag_func=None,
+	tags=('del', 'ins'),
+	merge=False,
+	word_separator_regex=r"\s+|[^\s\w]", # spaces and punctuation
+	cleanup_semantic=True,
+	):
+
 	# Serialize the text content of the two documents.
 	doc1data = serialize_document(doc1)
 	doc2data = serialize_document(doc2)
 
 	# Compute the differences in the serialized text.
-	diff = perform_diff(doc1data.text, doc2data.text)
+	diff = perform_diff(
+		doc1data.text, doc2data.text,
+		word_separator_regex=word_separator_regex,
+		cleanup_semantic=cleanup_semantic)
 	diff = simplify_diff(diff)
 	diff = reformat_diff(diff)
 
@@ -80,22 +91,21 @@ def serialize_document(doc):
 	# Return the serialized data.
 	return state
 
-def perform_diff(text1, text2):
+def perform_diff(text1, text2, word_separator_regex, cleanup_semantic):
 	# Do a word-by-word comparison, which produces more semantically sensible
 	# results. Remove the node_end_sentinel here.
 
 	word_map = { }
 
 	def text_to_words(text):
-		# The node_end_sentinel gets removed here. It is picked up by the
-		# second half of the regular expression since it is not a space
-		# or word character. So it acts as a word break. Then we discard it
-		# explicitly below.
-		words = re.split(r"(\s+|[^\s\w])", text)
+		# The node_end_sentinel gets removed here in the process of separating
+		# the text on word breaks.
+		words = re.split('(' + re.escape(node_end_sentinel) + '+|' + word_separator_regex + ')', text)
 		encoded_text = StringIO()
 		for wd in words:
-			if wd == "": continue # when there are multiple delimiters in a row, we may get blacks from re.split
+			if wd == "": continue # when there are multiple delimiters in a row, we may get blanks from re.split
 			if wd == node_end_sentinel: continue
+			if node_end_sentinel in wd: raise ValueError(wd)
 			wd_code = word_map.setdefault(wd, chr(255 + len(word_map)) )
 			encoded_text.write(wd_code)
 		return encoded_text.getvalue()
@@ -109,72 +119,92 @@ def perform_diff(text1, text2):
 	wdiff = diff_match_patch.diff(
 		text1, text2,
 		timelimit=0,
-        checklines=False)
+        checklines=False,
+        cleanup_semantic=cleanup_semantic)
 
 	# Map everything back to real characters.
 	word_map_inv = dict((v, k) for (k, v) in word_map.items())
 	diff = []
 	i1 = 0
 	i2 = 0
-	for op, woplen in wdiff:
-		oplen = 0
+	for op, oplen in wdiff:
 		if op == "-":
-			for i in range(woplen):
-				oplen += len(word_map_inv[text1[i1 + i]])
-			i1 += woplen
+			text = "".join(word_map_inv[text1[i1 + i]] for i in range(oplen) )
+			i1 += oplen
 		elif op == "+":
-			for i in range(woplen):
-				oplen += len(word_map_inv[text2[i2 + i]])
-			i2 += woplen
+			text = "".join(word_map_inv[text2[i2 + i]] for i in range(oplen) )
+			i2 += oplen
 		elif op == "=":
-			for i in range(woplen):
-				oplen += len(word_map_inv[text2[i2 + i]])
-			i1 += woplen
-			i2 += woplen
-		diff.append( (op, oplen) )
-
-	return diff
+			text = "".join(word_map_inv[text2[i2 + i]] for i in range(oplen) )
+			i1 += oplen
+			i2 += oplen
+		yield (op, text)
 
 	
 def simplify_diff(diff_iter):
 	# Simplify the diff by collapsing any regions with more changes than
 	# similarities, so that small unchanged regions appear within the larger
 	# set of changes (as changes, not as similarities).
+	
 	prev = []
-	for op, length in diff_iter:
-		if len(prev) < 2:
-			prev.append( (op, length) )
-		else:
-			# If the op two hunks ago is the same as the current hunk and
-			# the total lengths of two hunks ago and the current is creater
-			# than the length of the hunk in the middle...
-			if op in ('-', '+') and prev[0][0] == op and prev[1][0] == '=' \
-				and prev[1][1] > 0 and prev[0][1] + length > (prev[1][1]-1)**1.6:
-				prev.append( (op, prev[0][1] + prev[1][1] + length) )
-				prev.append( ('-' if op == '+' else '+', prev[1][1]) )
-				prev.pop(0)
-				prev.pop(0)
-				
-			# If the two hunks differ in op, combine them a different way.
-			elif op in ('-', '+') and prev[0][0] in ('-', '+') and prev[1][0] == '=' \
-				and prev[1][1] > 0 and prev[0][1] + length > (prev[1][1]-1)**1.6:
-				prev.append( (prev[0][0], prev[0][1] + prev[1][1]) )
-				prev.append( (op, prev[1][1] + length) )
-				prev.pop(0)
-				prev.pop(0)
+
+	def opposite_op(op):
+		return '-' if op == '+' else '+'
+
+	for op, text in diff_iter:
+		prev.append( (op, text) )
+
+		cycle = True
+		while cycle:
+			cycle = False
+
+			if len(prev) >= 3 and prev[-1][0] in ('-', '+') and prev[-2][0] == '=':
+				threshold = (len(prev[-2][1])-1)**2
+
+				if len(prev[-3][1]) + len(prev[-1][1]) > threshold:
+
+					# Replace <DEL:a> <EQ:b> <DEL:c> with <DEL:abc> <INS:b>
+					if prev[-3][0] == prev[-1][0]:
+						prev[-3] = (prev[-1][0], prev[-3][1] + prev[-2][1] + prev[-1][1])
+						prev[-2] = (opposite_op(prev[-1][0]), prev[-2][1])
+						prev.pop()
+						cycle = True
+
+					# If the two hunks differ in prev[-1][0], combine them a different way.
+					# Replace <DEL:a> <EQ:b> <INS:c> with <DEL:ab> <INS:bc>
+					elif prev[-3][0] == opposite_op(prev[-1][0]):
+						prev[-3] = (prev[-3][0], prev[-3][1] + prev[-2][1])
+						prev[-2] = (prev[-1][0], prev[-2][1] + prev[-1][1])
+						prev.pop()
+						cycle = True
+
+			# Because of the above, we may end up with alternating <DEL> <INS> <DEL>
+			# which we can collapse too.
+			# Replace <DEL:a> <INS:b> <DEL:c> with <DEL:ac> <INS:b>
+			if len(prev) >= 3 and prev[-1][0] in ('-', '+') and prev[-3][0] == prev[-1][0] and prev[-2][0] == opposite_op(prev[-1][0]):
+				prev[-3] = (prev[-3][0], prev[-3][1] + prev[-1][1])
+				prev.pop()
+				cycle = True
+
+			# And then replace two of the same ops in a row.
+			if len(prev) >= 2 and prev[-2][0] == prev[-1][0]:
+				prev[-2] = (prev[-1][0], prev[-2][1] + prev[-1][1])
+				prev.pop()
+				cycle = True
 			
-			else:
-				yield prev.pop(0)
-				prev.append( (op, length) )
+		while len(prev) >= 4:
+			yield prev.pop(0)
+
 	for p in prev:
 		yield p
 
 def reformat_diff(diff_iter):
 	# Re-format the operations of the diffs to indicate the byte
-	# offsets on the left and right rather than op and length.
+	# offsets on the left and right rather than op and text.
 	left_pos = 0
 	right_pos = 0
-	for op, length in diff_iter:
+	for op, text in diff_iter:
+		length = len(text)
 		left_len = length if op in ("-", "=") else 0
 		right_len = length if op in ("+", "=") else 0
 		yield (op, left_pos, left_len, right_pos, right_len)
